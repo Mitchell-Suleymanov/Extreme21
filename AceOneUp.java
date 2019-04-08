0ee911b32dd58bc3fc5c2afec2b944ea3926febb
Mitchell Suleymanov <MitchSuley@gmail.com>
2019-04-07 22:38:11 -0400

    Create AceOneUp.java

---
+++ AceOneUp.java
@@ -0,0 +1,56 @@
+package Aces;
+import Extreme21.Extreme21Game;
+
+public class AceOneUp extends Ace {
+	
+	//Target will help in remove function. True = Player, False = Opponent
+	boolean targetPlayer;
+
+	public AceOneUp(){
+		this.name = "One Up";
+		this.description = "Your opponent's bet is increased by 1 while this Ace is in play.";
+		this.isActive=true;
+	}
+	
+	@Override
+	public void use(Extreme21Game game){
+		game.setUpdateAcesInPlay(true);//flag as true so it is updated in the update() method for Extreme11Frame
+		
+		//Checks if it is currently the Player's turn.
+		if(game.getPlayerIsNext()){
+			targetPlayer= false;
+			int bet = game.getOpponent().getBet();
+			game.getOpponent().setBet(bet+1); //Player used OneUp, so increment Opponent's Bet by 1 
+			AceOneUp ace = new AceOneUp(); 
+			game.getPlayer().getAcesInPlay().add(ace); //Add OneUp to Player's list of acesInPlay
+			
+		}
+		else{
+			targetPlayer= true;
+			int bet = game.getPlayer().getBet();
+			game.getPlayer().setBet(bet+1); //Opponent used OneUp, so increment Player's Bet by 1 
+			AceOneUp ace = new AceOneUp();
+			game.getOpponent().getAcesInPlay().add(ace); //Add OneUp to Opponent's list of acesInPlay
+		}
+		
+		
+	}
+	
+	//When OneUp is removed from play, undo it's effects.
+	//target variable will determine who to adjust. True = Player, False = Opponent
+	//Effect undone: Reduce opponent's bet by 1
+	@Override
+	public void removeFromPlay(Extreme21Game game){
+		game.setUpdateAcesInPlay(true);//flag as true so it is updated in the update() method for Extreme11Frame
+		
+		//Checks target of the Ace's effects, then does the opposite since it is no longer in play. 
+		if(!targetPlayer){
+			int bet = game.getPlayer().getBet();
+			game.getPlayer().setBet(bet-1);
+		}
+		else{
+			int bet = game.getOpponent().getBet();
+			game.getOpponent().setBet(bet-1);
+		}
+	}
+}
